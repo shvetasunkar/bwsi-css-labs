5cafed0736ef589689ec0743e63aafcb37e3b93a
shvetasunkar <shveta.sunkar@gmail.com>
2026-03-30 19:48:31 -0400

    Update lab_1a.py
    added personal description

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Shveta" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hey everyone! I like coding and I am excited to be a part of this course.")
 
 if __name__ == "__main__":
     main()
